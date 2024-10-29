e9dd6ad98fca7d74cb3b9dc6bff471b300d778cc
‘MunirBsharaUSC’ <bshara@usc.edu>
2024-10-28 20:14:37 -0700

    initial scan attempt

--- scan/scan.cu
+++ scan/scan.cu
@@ -27,6 +27,32 @@ static inline int nextPow2(int n) {
     return n;
 }
 
+
+__global__ void
+ex_scan(int N, int* input, int*output)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+
+    for (int two_d = 1; two_d <= N/2; two_d*=2){
+        int two_dplus1 = 2*two_d;
+        int real_index = index*two_dplus1;
+        if (real_index < N){
+            output[real_index+two_dplus1-1] += output[real_index+two_d - 1];
+        }
+    }
+
+    output[N-1] = 0;
+
+    for (int two_d = N/2; two_d >= 1;  two_d/=2){
+        int two_dplus1 = 2*two_d;
+        int real_index = index*two_dplus1;
+        if(real_index < N){
+            int t = output[real_index+two_d -1];
+            output[real_index+two_d - 1] = output[real_index+two_dplus1 -1];
+            output[real_index+two_dplus1 - 1] +=t;
+        }
+    }
+}
 // exclusive_scan --
 //
 // Implementation of an exclusive scan on global memory array `input`,
@@ -53,8 +79,9 @@ void exclusive_scan(int* input, int N, int* result)
     // on the CPU.  Your implementation will need to make multiple calls
     // to CUDA kernel functions (that you must write) to implement the
     // scan.
-
-
+    int num_of_blocks = 1;
+    int threads_per_block = N/2;
+    ex_scan<<<num_of_blocks, threads_per_block>>>(N,input,result);
 }
 
 
